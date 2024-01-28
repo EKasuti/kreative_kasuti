42de93d4a3b0a0a0f9b2ecdcc2ffe5826a2268d2
Ekasuti <emmanuel.k.makau.jr.26@dartmouth.edu>
2024-01-28 04:52:19 -0500

    Updated portfolio section

---
+++ kreative_kasuti_ui/src/data/PortfolioData.jsx
@@ -0,0 +1,82 @@
+import Furaha from "../assets/images/furaha.jpg";
+import EasternBeauty from "../assets/images/eastern_beauty.jpg";
+import Horizon from "../assets/images/oil_painting.jpg";
+
+export const portfolioData = {
+  artCategory: [
+    {
+      id: 1,
+      category: "All",
+    },
+    {
+      id: 2,
+      category: "Nature",
+    },
+    {
+      id: 3,
+      category: "People",
+    },
+    {
+      id: 4,
+      category: "Abstract",
+    },
+    {
+      id: 5,
+      category: "3d models",
+    },
+  ],
+  myPortfolioData: [
+    {
+      id: 1,
+      title: "The horizon",
+      label: "Oil Painting",
+      category: "Nature",
+      author: "Kasuti",
+      imageUrl: Horizon,
+      altText: "Oil painting",
+      frameType: "Landscape",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+      id: 2,
+      title: "The horizon",
+      label: "Oil Painting",
+      category: "Nature",
+      author: "Kasuti",
+      imageUrl: Horizon,
+      altText: "Oil painting",
+      frameType: "Landscape",
+    },
+    {
+      id: 3,
+      title: "Furaha",
+      label: "Painting",
+      category: "People",
+      author: "Kasuti",
+      imageUrl: Furaha,
+      altText: "Furaha",
+      frameType: "Portrait",
+    },
+    {
+      id: 4,
+      title: "The horizon",
+      label: "Oil Painting",
+      category: "Nature",
+      author: "Kasuti",
+      imageUrl: Horizon,
+      altText: "Oil painting",
+      frameType: "Landscape",
+    },
+    {
+      id: 5,
+      title: "The horizon",
+      label: "Oil Painting",
+      category: "Nature",
+      author: "Kasuti",
+      imageUrl: Horizon,
+      altText: "Oil painting",
+      frameType: "Landscape",
+    },
+  ],
+};

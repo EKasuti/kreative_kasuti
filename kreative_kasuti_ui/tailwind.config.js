bf995c3b44aede4c259b88f01f4773150284d32b
Ekasuti <emmanuel.k.makau.jr.26@dartmouth.edu>
2024-02-14 04:01:16 -0500

    Fixed color scheme on footer

--- kreative_kasuti_ui/tailwind.config.js
+++ kreative_kasuti_ui/tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       'primary-button': '#FFFF00',
       'primary-text-color': '#FFFFFF',
       'primary-black': '#000000',
+      'dark-gray' : '#616161', // dark gray 700
+      'medium-gray' : '#C6D3E6', // medium gray 400
+      'light-gray' : '#C8C8C8C8', // light gray 200
     },
     extend: {},
   },
